550dff4ed118de6706f3ac607028e7a00734d206
Lorensov <lucianlorens@gmail.com>
2019-05-07 01:53:17 -0300

    :memo: add first route to swagger

--- api/app.py
+++ api/app.py
@@ -3,10 +3,10 @@ import sys
 import os
 
 from flask import Flask, request, jsonify, Response, send_from_directory, send_file
-#from flasgger import Swagger
-#from flasgger import swag_from
 
-#from swagger.swagger_config import swagger_configuration
+from flasgger import Swagger
+from flasgger import swag_from
+from swagger.swagger_config import swagger_configuration
 
 abs_path = os.path.dirname(os.path.realpath(__file__))
 abs_path = abs_path.replace("api", "job")
@@ -16,12 +16,13 @@ sys.path.append(abs_path)
 import runner
 
 app = Flask(__name__)
-#swagger = Swagger(app, config=swagger_configuration)
+swagger = Swagger(app, config=swagger_configuration)
 
-@app.route('/test', methods=['POST', 'GET'])
+@app.route('/test', methods=['GET'])
+@swag_from('swagger/test.yml')
 def test():
     print("Request on {} route at".format(request.url_rule), datetime.datetime.now())
-    return "TEST OK!"
+    return jsonify({'status': 'ok'})
 
 @app.route('/trigger', methods=['POST', 'GET'])
 def trigger():
@@ -46,8 +47,9 @@ def stop():
     data = runner_.stop_job()
     return jsonify(data)
 
-@app.route('/result', methods=['GET'])
+@app.route('/download', methods=['GET'])
 def result():
+    #
     print("Request on {} route at".format(request.url_rule), datetime.datetime.now())
     runner_ = runner.SingleRunner()
     tsv_file = runner_.download()
@@ -55,3 +57,16 @@ def result():
     app.logger.info(tsv_file)
     return send_from_directory('../', tsv_file, as_attachment=True, attachment_filename='output.tsv')
     #return send_file("../"+tsv_file, attachment_filename='output.tsv')
+"""
+#implement mode of showing on browser the .tsv 
+@app.route('/result', methods=['GET'])
+def result():
+    #
+    print("Request on {} route at".format(request.url_rule), datetime.datetime.now())
+    runner_ = runner.SingleRunner()
+    tsv_file = runner_.download()
+    print("/../"+tsv_file)
+    app.logger.info(tsv_file)
+    return send_from_directory('../', tsv_file, as_attachment=True, attachment_filename='output.tsv')
+    #return send_file("../"+tsv_file, attachment_filename='output.tsv')
+"""
